8bafef9734bf01649a095f43ec0acce9b69966c9
Rahul <rahul16086@gmail.com>
2022-01-22 19:10:15 +0000

    Added another loading component and fixed signup two form redirect

--- src/components/Auth/SignUpTwo.tsx
+++ src/components/Auth/SignUpTwo.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 import Button from "../Button/Button";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import LoaderSecond from "../Loader/LoaderSecond";
 
 const SignUpTwo: React.FC = () => {
   const locationRef = useRef() as React.RefObject<any>;
@@ -16,12 +17,14 @@ const SignUpTwo: React.FC = () => {
   const summaryRef = useRef() as React.RefObject<any>;
   const skillsRef = useRef() as React.RefObject<any>;
 
-  const navigate = useNavigate();
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [signUpDone, setSignUpDone] = useState(false);
 
   const signUpTwoHandler = async (event: { preventDefault: () => void }) => {
     event.preventDefault();
     try {
+      setLoading(true);
       const location = locationRef.current.value;
       const designation = designationRef.current.value;
       const experience = experienceRef.current.value;
@@ -73,9 +76,10 @@ const SignUpTwo: React.FC = () => {
       const resultJson = await result.json();
       console.log(resultJson);
 
-      if (resultJson.message) {
-        navigate("/app/matchmaker");
+      if (resultJson.success) {
+        setSignUpDone(true);
       }
+      setLoading(false);
     } catch (e: any) {
       console.log(e);
       setError(e && "Error occurred, Please check the values entered");
@@ -94,87 +98,108 @@ const SignUpTwo: React.FC = () => {
             <p>{error.toString()}</p>
           </div>
         )}
-        <div className={"signup__form__title"}>
-          <p>Your Info</p>
-        </div>
-        <div className={"signup__form__location"}>
-          <label>Location</label>
-          <input type={"text"} placeholder={"Enter City"} ref={locationRef} />
-        </div>
-        <div className={"signup__form__location"}>
-          <label>Designation</label>
-          <input
-            type={"text"}
-            placeholder={"Like software developer..."}
-            ref={designationRef}
-          />
-        </div>
-        <div className={"signup__form__location"}>
-          <label>Experience</label>
-          <input type={"text"} placeholder={"Years"} ref={experienceRef} />
-        </div>
-        <div className={"signup__form__github"}>
-          <label>GitHub</label>
-          <input type={"url"} ref={githubRef} />
-        </div>
-        <div className={"signup__form__portfolio"}>
-          <label>Portfolio</label>
-          <input type={"url"} ref={portfolioRef} />
-        </div>
-        <div className={"signup__form__linkedin"}>
-          <label>LinkedIn</label>
-          <input type={"url"} ref={linkedInRef} />
-        </div>
-        <div className={"signup__form__summary"}>
-          <label>Skills</label>
-          <textarea
-            placeholder={"Enter 6 skills separated by ' , '"}
-            ref={skillsRef}
-          />
-        </div>
-        <div className={"signup__form__summary"}>
-          <label>Summary</label>
-          <textarea
-            placeholder={"A short summary, about 30 words"}
-            ref={summaryRef}
-          />
-        </div>
-        <div className={"signup__form__title"}>
-          <p>Looking For</p>
-        </div>
-        <div className={"signup__form__lookingFor"}>
-          <label>Designation</label>
-          <select ref={lookingForRef}>
-            <option value={"front-end"}>Front-End Dev</option>
-            <option value={"back-end"}>Back-End Dev</option>
-            <option value={"full-stack"}>Full-Stack Dev</option>
-            <option value={"does"}>Does Programming</option>
-          </select>
-        </div>
-        <div className={"signup__form__remote"}>
-          <label>Remote Availability</label>
-          <select ref={remoteAvailabilityRef}>
-            <option value={"yes"}>Yes</option>
-            <option value={"no"}>No</option>
-          </select>
-        </div>
-        <div className={"signup__form__experience"}>
-          <label>Experience Level</label>
-          <select ref={experienceLevelRef}>
-            <option>Graduates</option>
-            <option>Low (1-2y)</option>
-            <option>Medium (2-5y)</option>
-            <option>High (5y +)</option>
-          </select>
-        </div>
-        <div className={"signup__form__radius"}>
-          <label>Match Radius (miles)</label>
-          <input type={"number"} ref={matchRadiusRef} />
-        </div>
+        {loading && (
+          <div className={"loader__container"}>
+            <LoaderSecond />
+          </div>
+        )}
+        {!loading && !signUpDone && (
+          <>
+            <div className={"signup__form__title"}>
+              <p>Your Info</p>
+            </div>
+            <div className={"signup__form__location"}>
+              <label>Location</label>
+              <input
+                type={"text"}
+                placeholder={"Enter City"}
+                ref={locationRef}
+              />
+            </div>
+            <div className={"signup__form__location"}>
+              <label>Designation</label>
+              <input
+                type={"text"}
+                placeholder={"Like software developer..."}
+                ref={designationRef}
+              />
+            </div>
+            <div className={"signup__form__location"}>
+              <label>Experience</label>
+              <input type={"text"} placeholder={"Years"} ref={experienceRef} />
+            </div>
+            <div className={"signup__form__github"}>
+              <label>GitHub</label>
+              <input type={"url"} ref={githubRef} />
+            </div>
+            <div className={"signup__form__portfolio"}>
+              <label>Portfolio</label>
+              <input type={"url"} ref={portfolioRef} />
+            </div>
+            <div className={"signup__form__linkedin"}>
+              <label>LinkedIn</label>
+              <input type={"url"} ref={linkedInRef} />
+            </div>
+            <div className={"signup__form__summary"}>
+              <label>Skills</label>
+              <textarea
+                placeholder={"Enter 6 skills separated by ' , '"}
+                ref={skillsRef}
+              />
+            </div>
+            <div className={"signup__form__summary"}>
+              <label>Summary</label>
+              <textarea
+                placeholder={"A short summary, about 30 words"}
+                ref={summaryRef}
+              />
+            </div>
+            <div className={"signup__form__title"}>
+              <p>Looking For</p>
+            </div>
+            <div className={"signup__form__lookingFor"}>
+              <label>Designation</label>
+              <select ref={lookingForRef}>
+                <option value={"front-end"}>Front-End Dev</option>
+                <option value={"back-end"}>Back-End Dev</option>
+                <option value={"full-stack"}>Full-Stack Dev</option>
+                <option value={"does"}>Does Programming</option>
+              </select>
+            </div>
+            <div className={"signup__form__remote"}>
+              <label>Remote Availability</label>
+              <select ref={remoteAvailabilityRef}>
+                <option value={"yes"}>Yes</option>
+                <option value={"no"}>No</option>
+              </select>
+            </div>
+            <div className={"signup__form__experience"}>
+              <label>Experience Level</label>
+              <select ref={experienceLevelRef}>
+                <option>Graduates</option>
+                <option>Low (1-2y)</option>
+                <option>Medium (2-5y)</option>
+                <option>High (5y +)</option>
+              </select>
+            </div>
+            <div className={"signup__form__radius"}>
+              <label>Match Radius (miles)</label>
+              <input type={"number"} ref={matchRadiusRef} />
+            </div>
 
-        <div className={"signup__form__submit"}>
-          <Button title={"Save"} type={"submit"} />
-        </div>
+            <div className={"signup__form__submit"}>
+              <Button title={"Save"} type={"submit"} />
+            </div>
+          </>
+        )}
+        {signUpDone && (
+          <div className={"signup__form__postSignup"}>
+            <p>User created successfully! Please login</p>
+            <Link to={"/login"}>
+              <Button title={"login"} />
+            </Link>
+          </div>
+        )}
       </form>
     </div>
   );
---
+++ src/components/Loader/LoaderSecond.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./LoaderSecond.css";
+
+const LoaderSecond = () => {
+  return (
+    <div className="lds-ripple">
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default LoaderSecond;
